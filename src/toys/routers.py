f69f9f8469ac1ad31984eb2f7b0ce07778aa4b78
Maystrenko <kultmet.dev@gmail.com>
2024-04-19 00:32:59 +0300

    remove comments

--- src/toys/routers.py
+++ src/toys/routers.py
@@ -9,29 +9,7 @@ from src.toys import crud, schemas, utils
 toy_router = routing.APIRouter(prefix="/toys", tags=["Toys"])
 
 
-@toy_router.get("/ex")
-def read_xlsx():
-    toy_types = {}
-    import numpy as np
-    import pandas
-
-    df = pandas.read_excel("files/detskiymir_msk_products.xlsx").replace(np.nan, None)
-    toys_data = df.to_dict(orient="records")
-    for toy in toys_data:
-        for k, v in toy.items():
-            if k not in toy_types:
-                toy_types[k] = set()
-            toy_types[k].add(type(v).__name__)
-    # print()
-    import json
-
-    print(json.dumps(toy_types, indent=2, default=str))
-    return toys_data[:10]
-
-
-@toy_router.get(
-    "", response_model=schemas.ToyPaginator
-)  # , response_model=schemas.ToyPaginator
+@toy_router.get("", response_model=schemas.ToyPaginator)
 async def featch_toys(
     request: Request,
     pagination: Annotated[schemas.Paginator, Depends(utils.paginate)],
